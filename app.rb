e336757fac0bcf24a79cc62a04a0f2a0919b02d4
alchambron <alexis.chambron@gmail.com>
2022-10-18 09:38:37 +0200

    First commit

---
+++ app.rb
@@ -0,0 +1,16 @@
+# Calling differents gems
+require 'bundler'
+Bundler.require
+
+# Calling diffents ruby files
+require_relative 'lib/user'
+require_relative 'lib/event'
+
+alexis = User.new("alchambron@gmail.com", 24)
+
+jour1 = Event.new("2022-10-17 15:10", 10, "standup quotidien", ["truc@machin.com", "bidule@chose.fr"])
+
+puts jour1.to_s
+
+puts alexis.find_by_email("alchambron@gmail.com")
+
---
+++ lib/event.rb
@@ -0,0 +1,43 @@
+class Event
+  attr_accessor :start_date, :duration, :title, :attendees
+
+  def initialize(start_date, duration, title, attendees)
+    @start_date = Time.parse(start_date)
+    @duration = duration.to_i
+    @title = title.to_s
+    @attendees = attendees
+  end
+
+  # Delaying by 24h
+  def postpone_24h
+    @start_date + 24 * 60 * 60
+  end
+
+  # Calculating ending time
+  def end_date
+    @start_date + @duration * 60
+  end
+
+  # Is the event past ?
+  def is_past?
+    @start_date < Time.now
+  end
+
+  # Is the event in the future ?
+  def is_future?
+    !is_past?
+  end
+
+  # Is the event soon ?
+  def is_soon?
+    Time.now > @start_date - 30 * 60 and Time.now < @start_date
+  end
+
+  def to_s
+    # Formating Time
+    puts "> Title : #{@title}"
+    puts "> Date de début : #{@start_date.strftime '%Y-%m-%d %H:%M'}"
+    puts "> Durée : #{@duration} minutes"
+    puts "> Invités : #{@attendees.join(', ')}"
+  end
+end
